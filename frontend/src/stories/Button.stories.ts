c06c7d61440f5ee20e1516fb58432451885e83e6
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-04 12:58:16 +1300

    test: storyboard testing for login button

--- frontend/src/stories/Button.stories.ts
+++ frontend/src/stories/Button.stories.ts
@@ -1,51 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import NavButton from "../components/buttons/NavButton";
 
-import { Button } from './Button';
-
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: 'Example/Button',
-  component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
-
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+const meta: Meta<typeof NavButton> = {
+    title: "NavButton",
+    component: NavButton,
+    parameters: {
+        layout:"centered",
+      },
+} satisfies Meta<typeof NavButton>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
+export const Basic: Story = {
+    args: { 
+        buttonType: "register" 
+    }
+}
 
-export const Secondary: Story = {
-  args: {
-    label: 'Button',
-  },
-};
+export const Signin: Story = {
+    args: { buttonType: "signin" }
+}
 
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
+export const Signout: Story = {
+    args: { buttonType: "signout" }
+}
