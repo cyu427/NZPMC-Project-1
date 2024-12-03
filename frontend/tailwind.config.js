b4ec4d62ffee3269bcfb005dbada41c1936e3528
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-03 16:19:56 +1300

    chore: Adding NZPMC theme colours to tailwind css config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,5 +5,15 @@ export default {
     extend: {},
   },
   plugins: [],
+  extend: {
+    colors: {
+      "nzpmc-blue": "#285DE5",
+      "nzpmc-yellow": "#FCC808",
+      "nzpmc-white": "#FFFFFF",
+      "nzpmc-black": "#000000",
+      "nzpmc-landing-footer-bg": "#28242C",
+      "nzpmc-default-bg": "#F0F4F4",
+    },
+  },
 }
 
