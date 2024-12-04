01acad62a738de1d7619e5c93add4438be997c76
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-04 17:13:56 +1300

    style: Better folder structure

---
+++ frontend/src/stories/components/icons/ExitIcon.stories.ts
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ExitIcon from "../../../components/icons/ExitIcon";
+
+const meta: Meta<typeof ExitIcon> = {
+    title: "Exit Icon",
+    component: ExitIcon,
+    parameters: {
+        layout:"centered",
+      },
+}
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {};
