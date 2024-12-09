bb675b2cc5223f114e7aa45ae5df993ba8586d2c
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-09 17:25:52 +1300

    feat: Created date time picker for date + time

---
+++ frontend/src/components/createEvent/form/EventDateTimeField.tsx
@@ -0,0 +1,31 @@
+import { CreateEventSchemaFormData } from "../../../schema/createEventSchema";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import dayjs from "dayjs";
+import { Control, Controller, FieldErrors } from "react-hook-form";
+
+const EventDateTimeField: React.FC<{
+    control: Control<CreateEventSchemaFormData>;
+    errors: FieldErrors<CreateEventSchemaFormData>;
+  }> = ({ control, errors }) => (
+    <Controller
+      name="dateTime"
+      control={control}
+      defaultValue=""
+      render={({ field }) => (
+        <DateTimePicker
+            label="Date & Time"
+            value={field.value ? dayjs(field.value) : null}
+            onChange={(newValue) => field.onChange(newValue?.toISOString())}
+            slotProps={{
+              textField: {
+                fullWidth: true,
+                margin: 'normal',
+                error: !!errors.dateTime,
+                helperText: errors.dateTime?.message,
+              },
+            }}
+          />
+      )}
+    />
+  )
+  export default EventDateTimeField;
