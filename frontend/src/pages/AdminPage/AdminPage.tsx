dbff4787ceb997aede5e85b20d4ac4273112f016
Cedric Yu <cyu427@aucklanduni.ac.nz>
2024-12-12 18:48:12 +1300

    feat: Events conditionally render on admin page

--- frontend/src/pages/AdminPage/AdminPage.tsx
+++ frontend/src/pages/AdminPage/AdminPage.tsx
@@ -3,91 +3,22 @@ import Navigation from "../../components/Navigation";
 import EventSection from "../../components/EventSection/EventSection";
 import Footer from "../../components/footer/Footer";
 import DataTableWithSearch from "../../components/table/DataTableWithSearch";
-import HeroSectionSignedIn from "../../components/HeroSectionSignedIn";
+import { useQuery } from "@tanstack/react-query";
+import { getAllEvents } from "../../queries/event";
 
 interface Event {
   id: string;
-  title: string;
-  date: string;
+  name: string;
+  dateTime: Date;
   location: string;
   cost: string;
 }
 
-const events: Event[] = [
-    {
-      id: '1',
-      title: 'NZPMC 2024 Round 1',
-      date: '30 Dec, 1pm',
-      location: 'University of Auckland',
-      cost: '13.50',
-    },
-    {
-      id: '2',
-      title: 'NZPMC 2024 Round 2',
-      date: '30 Dec, 1pm',
-      location: 'University of Auckland',
-      cost: '13.50',
-    },
-    {
-      id: '3',
-      title: 'NZPMC 2024 Round 3',
-      date: '30 Dec, 1pm',
-      location: 'University of Auckland',
-      cost: '13.50',
-    },
-    {
-      id: '4',
-      title: 'NZPMC 2024 Round 4',
-      date: '30 Dec, 1pm',
-      location: 'University of Auckland',
-      cost: '13.50',
-    },
-    {
-      id: '5',
-      title: 'NZPMC 2024 Round 5',
-      date: '31 Dec, 1pm',
-      location: 'University of Auckland',
-      cost: '13.50',
-    },
-    // Add more events as needed
-  ];
-
 const AdminPage: React.FC = () => {
-    // return (
-    //     <Box 
-    //       component="main"
-    //       sx={{ 
-    //         display: 'flex', 
-    //         flexDirection: 'column',
-    //         minHeight: '100vh', // Full height of the viewport
-    //       }}
-    //     >
-    //         <Navigation />
-    //         <Box 
-    //           sx={{ 
-    //             paddingTop: '30px',
-    //             width: '1200px',
-    //             dth: '100%',
-    //           }}
-    //         >
-    //           <EventSection events={events} title="All Events" /> 
-    //         </Box>
-    //         <Box 
-    //           sx={{
-    //             paddingTop: '30px',
-    //             maxWidth: '1200px',
-    //             flexDirection: 'column',
-    //             alignItems: 'center',
-    //             justifyContent: 'center',
-    //             mx: 'auto', // Margin auto for centering
-    //             paddingBottom: '30px',
-    //           }}
-    //         >
-    //           <DataTableWithSearch />
-    //         </Box>
-    //         <Footer />
-    //     </Box>
-    // );
+  const { data: events } = useQuery<Event[], Error>({
+    queryKey: ['allEvents'],
+    queryFn: getAllEvents,
+  });
 
     return (
     <Box 
