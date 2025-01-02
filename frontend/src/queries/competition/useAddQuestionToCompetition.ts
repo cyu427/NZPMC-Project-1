674bc6870aa2bbc486ec8dd0b54a4a3b78a16c0c
Cedric Yu <cyu427@aucklanduni.ac.nz>
2025-01-02 21:11:18 +1300

    feat: Added query hook for adding question to competition

--- frontend/src/queries/competition/useAddQuestionToCompetition.ts
+++ frontend/src/queries/competition/useAddQuestionToCompetition.ts
@@ -1,14 +1,17 @@
 import { useMutation, } from "@tanstack/react-query";
 import axios from "axios";
 import { CreateCompetitionFormData } from "../../schema/formValidation/competitionSchema";
+import { Option } from "../../context/AnswerContext";
 
 interface AddQuestionToCompetitionParams {
     competitionId: string;
     questionId: string;
+    attempt: Record<string, Option>
+
   }
 
-const addQuestionToCompetition = ({competitionId, questionId}: AddQuestionToCompetitionParams) =>
-    axios.put(`http://localhost:8080/admin/competition/addQuestionToCompetition/${competitionId}/${questionId}`);
+const addQuestionToCompetition = ({competitionId, questionId, attempt}: AddQuestionToCompetitionParams, ) =>
+    axios.put(`http://localhost:8080/admin/competition/addQuestionToCompetition/${competitionId}/${questionId}`, attempt);
 
 export const useAddQuestionToCompetition = () => {
     // Return useMutation hook with createQuestion as the mutation function
